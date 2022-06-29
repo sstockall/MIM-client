01be13c0617af4b7e4d7a88ec2a4eca93d379b54
Samantha Stockall <sam.stockall@hotmail.com>
2022-06-28 20:31:13 -0400

    Accidentally working on main

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,9 +2,17 @@ import './HomePage.scss'
 
 function HomePage() {
     return ( 
-        <section className="home">
+        <main className="home">
+            <section className='home__hero'>
+                <h1 className='home__hero-header'>Let Us Worry For You</h1>
+                <p className='home__hero-text'>Keeping track of your moles is stressful.</p>
+                <p className='home__hero-text'>Some would say it's a full time job.</p>
+                <p className='home__hero-text'>So why not let <span className='home__hero-text--mim'>MIM</span> do it for you?</p>
+            </section>
+            <section className='home__background'>
 
-        </section>
+            </section>
+        </main>
      );
 }
 
